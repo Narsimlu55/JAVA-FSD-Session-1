0ead7dddce5cdc72acf9048d3c0a14ec300c06f1
kurma narsimlu <kurma narsimlu>
2022-01-25 23:02:58 +0530

    Session-1

---
+++ src/com/simplilearn/lvc/CheckAccessinSamePakage.java
@@ -0,0 +1,12 @@
+package com.simplilearn.lvc;
+
+public class CheckAccessinSamePakage {
+public static void main(String[] args) {
+		
+		AccessSpecifier obj=new AccessSpecifier();
+		obj.display();//able to call protected method
+		obj.test();//not able to call private method
+		obj.hello();//able to call public method
+		obj.world();//able to call default method
+	}
+}
